2873ba4d2387ca2ff857b6e16d011802abfe2623
razpataz <trond.halvorsen79@gmail.com>
2015-05-27 21:54:35 +0200

    Vedlikeholds app
    Alle funskjonene på plass

--- vedlikehold/index.php
+++ vedlikehold/index.php
@@ -57,8 +57,8 @@ $dbApi = new Database();
                  }
                 echo ("<td><input type='submit' value='Edit' name='editknapp' id='editknapp'>
                         <input type='submit' value='Delete' name='deleteknapp' id='deleteknapp'>
-                        <input type='text' size='10' type='text' name='rowID' value='" . $row[0] . "'>
-                        <input type='text' size='10' type='text' name='table_name' value='$valgt_table'>
+                        <input type='hidden' size='10' type='text' name='rowID' value='" . $row[0] . "'>
+                        <input type='hidden' size='10' type='text' name='table_name' value='$valgt_table'>
                       </td></form></tr>");
             }
             print ("<tr><form action='' method='post' id='addForm' name='addForm'><td></td>");
@@ -155,16 +155,106 @@ $dbApi = new Database();
         @$editknapp=$_POST ["editknapp"];
                 if ($editknapp)
                     {
-                    $ID = $_POST['rowID'];
+                    $id = $_POST['rowID'];
                     $valgt_table=$_POST ["table_name"];
-                    print ("Here is where you edit the selected row $ID<br>");
+
+                    $result = $dbApi->getRow($valgt_table, $id);
+                    
+
+                    $column = array();
+            $result = $dbApi->getColumnNames("$valgt_table");
+            print("<table><tr>");
+            while($row = mysqli_fetch_row($result)) 
+            {
+                echo("<th>" . $row[0] . "</th>");
+                $column[]=$row[0]; 
+            }
+            print("</tr>");
+            print("<tr>");
+            $result = $dbApi->getRow($valgt_table, $id);
+
+            while($row = mysqli_fetch_row($result)) 
+            {
+                echo ("<tr><form action='' method='post' id='EditForm' name='EditForm'>");
+                $rows=count($column);
+                $nr=0;
+                for ($x=1;$x<=$rows;$x++)
+                 {
+                    if ($column[$nr]=="RoomTypeID" || $column[$nr]=="ImageID" || $column[$nr]=="HotelRoomTypeID" || $column[$nr]=="HotelID"  || $column[$nr]=="CustomerOrderID" || $column[$nr]=="RoomID") 
+                        {
+                        $foreigntable= substr($column[$nr], 0, -2);
+                        print ("<td><select name=$nr type='input'>");
+                        $result2 = $dbApi->getAllRows("$foreigntable");
+                        while($row2 = mysqli_fetch_row($result2)) 
+                            {
+                            echo("<option value='" . $row2[0] . "'>" . $row2[0] . "</option>");
+                            }
+                        print ("</select></td>");
+                        }
+                    else 
+                        {
+                            echo ("<td><input type='text' size='10' type='text' name='$nr' value='" . $row["$nr"] . "'></td>");
+                        }
+
+                        $nr++;
+
+                 }
+                echo ("<td><input type='submit' value='Update' name='updateknapp' id='updateknapp'>
+                        <input type='hidden' size='10' type='text' name='rowID' value='" . $row[0] . "'>
+                        <input type='hidden' size='10' type='text' name='table_name' value='$valgt_table'>
+                      </td></form></tr>");
+            }
+                    print ("Here is where you edit the selected row $id<br>");
                 }
        
 
-
-
-
-
+                @$updateknapp=$_POST ["updateknapp"];
+                if ($updateknapp)
+                    {
+                    $valgt_table=$_POST ["table_name"];
+                    $id = $_POST['rowID'];
+                    $column = array();
+                    $input = array();
+                    $result = $dbApi->getColumnNames("$valgt_table");
+                        while($row = mysqli_fetch_row($result)) {
+                                $column[]=$row[0];
+                        }
+                    $rows=count($column);
+                    $nr="1";
+                    for ($x=1;$x<=$rows;$x++)
+                            {
+                            $input[] = $_POST[$nr];
+                         $nr++;
+                         }
+                    print ("Table: $valgt_table<br>");
+                    $nr="1";
+                    $nr2="0";
+                    print("$column[$nr] = $input[$nr2]<br>");
+                    for ($x=2;$x<=$rows;$x++)
+                         {
+                         print("$column[$nr] = $input[$nr2]<br>");
+                         $nr++;
+                          $nr2++;
+                          }
+                    
+                    $nr="1";
+                    $nr2="0";
+                    $data = array();
+                    for ($x=2;$x<=$rows;$x++)
+                            {
+                            $data[$column[$nr]] = $input[$nr2];
+                         $nr++;
+                         $nr2++;
+                         }
+                    print_r($data);
+                    $result = $dbApi->updateRow($valgt_table, $id, $data);
+                    if($result) {
+                        echo("<br><span style='color:limegreen'>Successfully updated row in table $valgt_table!</span>");
+                    }
+                    else {
+                        echo("<br><span style='color:red'><strong>Updating of row into table $valgt_table FAILED!</strong></span>");
+                    }
+                }
 
 
 
