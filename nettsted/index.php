7268dc5cae04b649d2c6a10429f94db24cbe664e
Vecc <lol>
2015-05-28 16:21:09 +0200

    Fikset struktur på nettsted

---
+++ nettsted/index.php
@@ -0,0 +1,11 @@
+<?php require_once("template/start.html") ?>
+
+<!-- Start of content -->
+<div id="content">
+    <div id="innholdLeft"> </div>
+
+    <div id="innholdRight"> </div>
+</div>
+<!-- End of content -->
+
+<?php require_once("template/end.html") ?>
