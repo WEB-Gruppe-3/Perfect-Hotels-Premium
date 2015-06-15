18bbf01ed5f65712d93d300aadc05d3326e56090
Vecc <lol>
2015-06-15 02:26:15 +0200

    La til JS validering ved Enter press i inputs

--- vedlikehold/js/login.js
+++ vedlikehold/js/login.js
@@ -34,16 +34,38 @@ passInput.blur(function() {
 });
 
 // --------------------------------- Validating ---------------------------------
+// Validating on button click
 submitButton.click(function(event) {
-
-    // Check if inputs are empty or default
-    if( userInput.val() === "" || userInput.val() === defaultUserInputVal ||
-        passInput.val() === "" || passInput.val() === defaultPassInputVal) {
-
+    if(isInputsEmpty()) {
         event.preventDefault();
-        var error = $("#emptyInputErrorMessage");
-        error.html("Både brukernavn og passord må fylles ut!");
-        error.show();
+        showErrorIfEmptyInputs();
     }
-
 });
+
+// Validating on inputs-enter-press
+userInput.keyup(function(event) {
+    // Is the enter key pressed? (key 13)
+   if(event.keyCode === 13) {
+       if(isInputsEmpty()) {
+           event.preventDefault();
+           showErrorIfEmptyInputs();
+       }
+   }
+});
+
+/**
+ * Check if the inputs are empty or have the default value.
+ * @return boolean Returns true if they are empty, false otherwise.
+ */
+var isInputsEmpty = function() {
+    return (userInput.val() === "" ||
+            userInput.val() === defaultUserInputVal ||
+            passInput.val() === "" ||
+            passInput.val() === defaultPassInputVal)
+};
+
+var showErrorIfEmptyInputs = function() {
+    var error = $("#emptyInputErrorMessage");
+    error.html("Både brukernavn og passord må fylles ut!");
+    error.show();
+};
