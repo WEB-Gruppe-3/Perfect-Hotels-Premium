eaa8022c4adc21596ea541163d4e89884e81d7b4
Vecc <lol>
2015-06-16 13:02:47 +0200

    Validering på Endre Bestilling

--- nettsted/js/editorder.js
+++ nettsted/js/editorder.js
@@ -74,6 +74,21 @@ $(function() {
         requestNumberOfAvailableRooms()
     });
 
+    // Setting onClick for OK button for referanse input field
+    var okButton = $("[name=checkinbutton]");
+    var refInput = $("[name=search]");
+
+    okButton.click(function(event) {
+        if(refInput.val() === "" || refInput.val() === " ") {
+            event.preventDefault();
+            refInput.css("background-color", "red");
+        }
+        else {
+            refInput.css("background-color", "green");
+        }
+    });
+
+
 });
 /* ########################### SCRIPT END ########################### */
 
